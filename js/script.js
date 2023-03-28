919014dafdcc98947c1de46e141081cf48eaa6e8
junior bikcreft j <juniordejesus_batera@outlook.com>
2023-03-28 20:21:45 -0300

    otimizado para producao

---
+++ js/script.js
@@ -0,0 +1,69 @@
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+links.forEach(ativarLink);
+
+//ativar itens do orcamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+//perguntas frenquentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventoPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventoPerguntas);
+
+//galeria de bicicleta
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContener = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)");
+  console.log(media);
+  if (media) {
+    galeriaContener.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+//animation
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
+
+
